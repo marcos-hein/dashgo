c53adba56cb38482c28fa3a8a29a020dc0959b4b
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-10-22 14:57:51 -0300

    Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
-export function Profile(): JSX.Element {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps): JSX.Element {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Marcos V. Pereira</Text>
-        <Text color="gray.300" fontSize="small">
-          vinicius.hein@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Marcos V. Pereira</Text>
+          <Text color="gray.300" fontSize="small">
+            vinicius.hein@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Marcos V. Pereira" src="https://github.com/marcos-hein.png" />
     </Flex>
