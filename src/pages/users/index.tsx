c9238fa4e4c79cd27df1b116ee19f8b1d2df308c
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-06-30 17:40:56 -0300

    :sparkles: configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,25 +13,314 @@ import {
   Checkbox,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Link from "next/link";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
 export default function UserList(): JSX.Element {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     md: true,
   });
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/users")
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, []);
+  function renderError(): JSX.Element {
+    return (
+      <Flex justify="center">
+        <Text>Falha ao obter dados dos usuários.</Text>
+      </Flex>
+    );
+  }
+
+  function renderContent(): JSX.Element {
+    return isLoading ? (
+      <Flex justify="center">
+        <Spinner />
+      </Flex>
+    ) : (
+      <>
+        <Table colorScheme="whiteAlpha">
+          <Thead>
+            <Tr>
+              <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                <Checkbox colorScheme="pink" />
+              </Th>
+              <Th>Usuários</Th>
+              {isWideVersion && <Th>Data de cadastro</Th>}
+              <Th width="8" />
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Marcos Pereira</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    vinicius.hein@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              {isWideVersion && <Td>21 de Outubro, 2021</Td>}
+              <Td>
+                {isWideVersion && (
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    variant="ghost"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                )}
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+        <Pagination />
+      </>
+    );
+  }
 
   return (
     <Box>
@@ -58,273 +347,7 @@ export default function UserList(): JSX.Element {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuários</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8" />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcos Pereira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      vinicius.hein@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>21 de Outubro, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      variant="ghost"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {error ? renderError() : renderContent()}
         </Box>
       </Flex>
     </Box>
