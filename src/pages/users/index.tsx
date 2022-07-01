a783fba90a37440bf742f43a59320f18e8e13260
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-07-01 11:16:55 -0300

    :dizzy: flagging data refetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
 export default function UserList(): JSX.Element {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -127,6 +127,7 @@ export default function UserList(): JSX.Element {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
